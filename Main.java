0288eb2f54dafaed92d35aa70162c735996bfb95
BjerregaardGG <oliverbjerregaard1@gmail.com>
2024-04-28 14:54:57 +0200

    Changes

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        int age;
+        int height;
+
         for(int i=0; i<5; i++)
             System.out.println("Jeg hedder Oliver");
+
+
 
     }
 }
